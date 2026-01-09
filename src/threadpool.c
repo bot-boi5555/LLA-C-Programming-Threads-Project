21def4a994be7c5242c1881a00618d1e630f6052
bot-boi5555 <87390706+bot-boi5555@users.noreply.github.com>
2026-01-09 21:56:56 +0300

    Update temporarily threadpool.c

--- src/threadpool.c
+++ src/threadpool.c
@@ -3,7 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "threadpool.h"
+//#include "threadpool.h"
+
+#include <pthread.h>
+
+#define THREADS 16
+#define QUEUE_SIZE 100
+
+typedef struct {
+	void (*fn)(void* arg);
+	void* arg;
+
+} task_t;
+
+typedef struct {
+	pthread_mutex_t lock;
+	pthread_cond_t notify;
+	pthread_t threads[THREADS];
+	task_t task_queue[QUEUE_SIZE];
+	int queued;
+	int queue_front;
+	int queue_back;
+	int stop;
+
+} threadpool_t;
 
 void* threadpool_function(void* threadpool) {
 
